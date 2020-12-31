b5f7613f2d4e66aee2caa59eec85493f0a20659b
rft50 <62362611+rft50@users.noreply.github.com>
2020-12-30 20:24:05 -0600

    Add listeners to the tab manager

--- script/tabmanager.js
+++ script/tabmanager.js
@@ -1,6 +1,9 @@
 "use strict";
 
 let tabSet = [];
+let activeTab = [];
+let tabOpenListeners = [];
+let tabCloseListeners = [];
 
 // discovery
 
@@ -14,6 +17,9 @@ let tabSet = [];
 		}
 
 		tabSet[i] = [];
+		activeTab[i] = 0;
+		tabOpenListeners[i] = [];
+		tabCloseListeners[i] = [];
 		j = 0;
 		while (true) {
 			elem = document.getElementById(`Tab${i}_${j}`);
@@ -41,8 +47,51 @@ for (var i = 0; i < tabSet.length; i++) {
 
 function SwitchTab(tabGroup, tabId) {
 	let tabs = tabSet[tabGroup];
+	let opens = tabOpenListeners[tabGroup];
+	let closes = tabCloseListeners[tabGroup];
 
+	for (let i = 0; i < closes.length; i++) {
+		if (closes[i][1] == activeTab[tabGroup]) {
+			closes[i][0]()
+		}
+	}
 	for (let i = 0; i < tabs.length; i++) {
 		tabs[i].hidden = i != tabId; // hides all tabs, aside from the selected one
 	}
+	activeTab[tabGroup] = tabId
+	for (let i = 0; i < opens.length; i++) {
+		if (opens[i][1] == tabId) {
+			opens[i][0]()
+		}
+	}
+}
+
+// define adding and removing tab change listeners
+
+function AddTabOpenedListener(listener, tabSetId, tabId) {
+	tabOpenListeners[tabSetId].push([listener, tabId]);
+}
+
+function RemoveTabOpenedListener(listener, tabSetId) {
+	for (let i = 0; i < tabOpenListeners[tabSetId].length; i++) {
+		if (tabOpenListeners[tabSetId][0] === listener) {
+			tabOpenListeners[tabSetId].splice(i, 1)
+			return;
+		}
+	}
+	return;
+}
+
+function AddTabClosedListener(listener, tabSetId, tabId) {
+	tabCloseListeners[tabSetId].push([listener, tabId]);
+}
+
+function RemoveTabClosedListener(listener, tabSetId) {
+	for (let i = 0; i < tabCloseListeners[tabSetId].length; i++) {
+		if (tabCloseListeners[tabSetId][0] === listener) {
+			tabCloseListeners[tabSetId].splice(i, 1)
+			return;
+		}
+	}
+	return;
 }
